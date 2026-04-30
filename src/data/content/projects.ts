e5a53bde7abc7f47839182cf79cf204901cc60b5
xogus3492 <wkdxogus3492@gmail.com>
2026-04-30 14:32:40 +0900

    feat: GitHub 링크를 GitHub 아이콘 이미지로 교체

--- src/data/content/projects.ts
+++ src/data/content/projects.ts
@@ -19,7 +19,7 @@ export const REHAB_CENTER_CONTENT = `# 재활센터 홈페이지
 
 ## 링크
 
-- 🔗 [GitHub](https://github.com/xogus3492/sm-rehabilitation-center-website)
+- 🔗 [![](/icons/github.png)](https://github.com/xogus3492/sm-rehabilitation-center-website)
 - 🚀 [Live Site](https://sm-rehabilitation-center.vercel.app/)
 `;
 
@@ -63,8 +63,7 @@ Toss Payments API 기반으로
 결제 전 정보를 서버에 저장 → 승인 후 비교 검증하는 플로우를 설계하여 위변조 방지
 
 ## 링크
-
-- 🔗 [GitHub](https://github.com/little-bank/littlebank-server)
+🔗 [![](/icons/github.png)](https://github.com/little-bank/littlebank-server)
 `;
 
 export const DEVHUB_CONTENT = `# DEVHUB
@@ -108,7 +107,7 @@ DB 락(Lock)을 활용하여 동시 요청에 의한 데이터 정합성 문제 
 
 ## 링크
 
-- 🔗 [GitHub](https://github.com/Devs-Of-Kosmo/devhub-server)
+- 🔗 [![](/icons/github.png)](https://github.com/Devs-Of-Kosmo/devhub-server)
 - 📄 [Notion 문서](https://wheat-eustoma-8a4.notion.site/DEVHUB-40f17eb25bf84bd8ba87caa17c444d2b)
 `;
 
@@ -145,7 +144,7 @@ export const BOARD_CONTENT = `# 게시판 프로젝트
 
 ## 링크
 
-- 🔗 [GitHub](https://github.com/Cupid-Arrow-team/Board/tree/develop)
+- 🔗 [![](/icons/github.png)](https://github.com/Cupid-Arrow-team/Board/tree/develop)
 `;
 
 export const FYB_CONTENT = `# FYB (Fit Your Balance)
@@ -176,6 +175,6 @@ export const FYB_CONTENT = `# FYB (Fit Your Balance)
 
 ## 링크
 
-- 🔗 [GitHub](https://github.com/xogus3492/Front_Android)
+- 🔗 [![](/icons/github.png)](https://github.com/xogus3492/Front_Android)
 - 📄 [Notion 문서](https://wheat-eustoma-8a4.notion.site/FYB-1402cd91589f4e5fb177c0e85b31d4c1)
 `;
